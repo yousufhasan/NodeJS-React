134ff79b5361d1dda412a538cd7e7cd804944bc8
Yousuf Hasan <yousuf85@gmail.com>
2020-08-03 00:27:55 +1000

    Update App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,7 +5,7 @@ import { Order } from "./Order";
 function App() {
   return (
     <div className="container">
-      <h3 className="pt-3 pb-3 text-center"> AmazingCo Coding Challenge </h3>
+      <h3 className="pt-3 pb-3 text-center"> Discounted Products </h3>
       <Events />
       <Order />
     </div>
